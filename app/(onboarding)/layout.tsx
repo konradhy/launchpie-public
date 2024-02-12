6c9926a06dbcedfbdfecff283df53d5ee09bc27d
Konrad Hylton <konradhylton@gmail.com>
2024-02-11 22:11:32 -0500

    Add Suspense for lazy loading in MainLayout component

--- app/(onboarding)/layout.tsx
+++ app/(onboarding)/layout.tsx
@@ -1,12 +1,13 @@
 "use client";
-
+import { Suspense } from 'react' 
 
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
+      <Suspense fallback={<div>Loading...</div>} >
       {children}
-
+</Suspense>
     </main>
   );
 };
