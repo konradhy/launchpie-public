ba27b3b3a1d1d8f095cb963284e4df934f641559
Konrad Hylton <konradhylton@gmail.com>
2024-02-11 21:26:47 -0500

    Add MainLayout component to onboarding module

---
+++ app/(onboarding)/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      {children}
+
+    </main>
+  );
+};
+
+export default MainLayout;
