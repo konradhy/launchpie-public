8286e251527770739111135fe3c791d1bf81fc0c
Konrad Hylton <konradhylton@gmail.com>
2024-02-17 11:29:17 -0500

    Comment out Link component and its dependencies in link.tsx

--- components/typography/link.tsx
+++ components/typography/link.tsx
@@ -1,11 +1,10 @@
-// @ts-ignore
-import { se } from "@/lib/utils";
-import { AnchorHTMLAttributes } from "react";
+// import { se } from "@/lib/utils";
+// import { AnchorHTMLAttributes } from "react";
 
-export const Link = se<
-  HTMLAnchorElement,
-  AnchorHTMLAttributes<HTMLAnchorElement>
->(
-  "a",
-  "font-medium text-primary underline underline-offset-4 hover:no-underline",
-);
+// export const Link = se<
+//   HTMLAnchorElement,
+//   AnchorHTMLAttributes<HTMLAnchorElement>
+// >(
+//   "a",
+//   "font-medium text-primary underline underline-offset-4 hover:no-underline",
+// );
