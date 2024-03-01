9f23bb03eaafb27c6b531e0ff652b79b09df43c8
Konrad Hylton <konradhylton@gmail.com>
2024-03-01 15:13:23 -0500

    Update Error component to handle and display detailed error messages

--- app/error.tsx
+++ app/error.tsx
@@ -3,9 +3,26 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { ConvexError } from "convex/values";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 
-const Error = () => {
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  const errorMessage =
+    error instanceof ConvexError
+      ? (error.data as { message: string }).message
+      : "Unexpected error occurred";
   return (
     <div className="h-screen flex flex-col items-center justify-center space-y-4">
       <Image
@@ -23,11 +40,14 @@ const Error = () => {
         className="hidden dark:block"
       />
       <h2 className="text-xl font-medium">
-        Something went wrong! Please try again
+        Something went wrong: {errorMessage}
       </h2>
-      <Button asChild>
-        <Link href="/">Go back</Link>
-      </Button>
+      <div className="flex gap-2">
+        <Button onClick={() => reset()}>Try again</Button>
+        <Button asChild>
+          <Link href="/">Go Home</Link>
+        </Button>
+      </div>
     </div>
   );
 };
